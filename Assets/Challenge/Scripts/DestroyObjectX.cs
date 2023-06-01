5bf4a6224539b106a5a878970ae5f4c65cd68b48
Kamila <kamila.mspychala@vp.pl>
2023-06-01 19:00:22 +0200

    zadanko

---
+++ Assets/Challenge/Scripts/DestroyObjectX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+
+        // destroy particle after delay
+        Destroy(gameObject, 2);
+    }
+
+    
+
+
+}
